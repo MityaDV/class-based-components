7adda8110932077f60b01bbb52a9e08b184b4537
Dmitry Kovalkov <dmitriy.k@kultprosvet.net>
2025-09-30 13:58:22 +0300

    147. Работа с Состояниями и Событиями

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,37 +1,73 @@
-import { useState } from "react";
-import Customer from "./Customer";
+import { Component, useState } from 'react';
+import Customer from './Customer';
 
-import styles from "./Customers.module.css";
+import styles from './Customers.module.css';
 
 const DUMMY_CUSTOMERS = [
-  { id: "c1", name: "Дмитрий" },
-  { id: "c2", name: "Михаил" },
-  { id: "c3", name: "Ирина" },
+  { id: 'c1', name: 'Дмитрий' },
+  { id: 'c2', name: 'Михаил' },
+  { id: 'c3', name: 'Ирина' }
 ];
 
-const Customers = () => {
-  const [showCustomers, setShowCustomers] = useState(true);
+class Customers extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showCustomers: true,
+      someState: 'Foo'
+    };
+  }
 
-  const toggleCustomersHandler = () => {
-    setShowCustomers((curState) => !curState);
-  };
+  toggleCustomersHandler() {
+    // this.state.showCustomers = false - НЕПРАВИЛЬНО
+    this.setState((curState) => {
+      return { showCustomers: !curState.showCustomers };
+    });
+  }
 
-  const customersList = (
-    <ul>
-      {DUMMY_CUSTOMERS.map((customer) => (
-        <Customer key={customer.id} name={customer.name} />
-      ))}
-    </ul>
-  );
+  render() {
+    const customersList = (
+      <ul>
+        {DUMMY_CUSTOMERS.map((customer) => (
+          <Customer key={customer.id} name={customer.name} />
+        ))}
+      </ul>
+    );
 
-  return (
-    <div className={styles.customers}>
-      <button onClick={toggleCustomersHandler}>
-        {showCustomers ? "Спрятать" : "Показать"} Заказчиков
-      </button>
-      {showCustomers && customersList}
-    </div>
-  );
-};
+    return (
+      <div className={styles.customers}>
+        <button onClick={this.toggleCustomersHandler.bind(this)}>
+          {this.state.showCustomers ? 'Спрятать' : 'Показать'} Заказчиков
+        </button>
+        {this.state.showCustomers && customersList}
+      </div>
+    );
+  }
+}
+
+// const Customers = () => {
+//   const [showCustomers, setShowCustomers] = useState(true);
+//
+//   const toggleCustomersHandler = () => {
+//     setShowCustomers((curState) => !curState);
+//   };
+//
+//   const customersList = (
+//     <ul>
+//       {DUMMY_CUSTOMERS.map((customer) => (
+//         <Customer key={customer.id} name={customer.name} />
+//       ))}
+//     </ul>
+//   );
+//
+//   return (
+//     <div className={styles.customers}>
+//       <button onClick={toggleCustomersHandler}>
+//         {showCustomers ? 'Спрятать' : 'Показать'} Заказчиков
+//       </button>
+//       {showCustomers && customersList}
+//     </div>
+//   );
+// };
 
 export default Customers;
