c2804a2ac8599fe0716dcb377e4ce1699c91256d
Dmitry Kovalkov <dmitriy.k@kultprosvet.net>
2025-09-30 16:02:55 +0300

    149. Life-cycle Методы | componentDidUpdate

--- src/components/CustomerFilter.js
+++ src/components/CustomerFilter.js
@@ -10,77 +10,77 @@ const DUMMY_CUSTOMERS = [
   { id: 'c3', name: 'Ирина' }
 ];
 
-// class CustomerFilter extends Component {
-//   static contextType = CustomersContext;
-//
-//   constructor() {
-//     super();
-//     this.state = {
-//       filteredCustomers: [],
-//       filter: ''
-//     };
-//   }
-//
-//   componentDidMount() {
-//     // Отправить HTTP запрос...
-//     this.setState({
-//       filteredCustomers: this.context.customers
-//     });
-//   }
-//
-//   componentDidUpdate(prevProps, prevState) {
-//     if (prevState.filter !== this.state.filter) {
-//       this.setState({
-//         filteredCustomers: this.context.customers.filter((customer) =>
-//           customer.name.includes(this.state.filter)
-//         )
-//       });
-//     }
-//   }
-//
-//   filterHandler(event) {
-//     this.setState({
-//       filter: event.target.value
-//     });
-//   }
-//
-//   render() {
-//     return (
-//       <Fragment>
-//         <div className={styles.filter}>
-//           <input type="search" onChange={this.filterHandler.bind(this)} />
-//         </div>
-//         <ErrorBoundary>
-//           <Customers customers={this.state.filteredCustomers} />
-//         </ErrorBoundary>
-//       </Fragment>
-//     );
-//   }
-// }
+class CustomerFilter extends Component {
+  //   static contextType = CustomersContext;
 
-const CustomerFilter = () => {
-  const [filteredCustomers, setFilteredCustomers] = useState(DUMMY_CUSTOMERS);
-  const [filter, setFilter] = useState('');
+  constructor() {
+    super();
+    this.state = {
+      filteredCustomers: DUMMY_CUSTOMERS,
+      filter: ''
+    };
+  }
 
-  useEffect(() => {
-    setFilteredCustomers(
-      DUMMY_CUSTOMERS.filter((customer) => customer.name.includes(filter))
+  //   componentDidMount() {
+  //     // Отправить HTTP запрос...
+  //     this.setState({
+  //       filteredCustomers: this.context.customers
+  //     });
+  //   }
+  //
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.filter !== this.state.filter) {
+      this.setState({
+        filteredCustomers: DUMMY_CUSTOMERS.filter((customer) =>
+          customer.name.includes(this.state.filter)
+        )
+      });
+    }
+  }
+
+  filterHandler(event) {
+    this.setState({
+      filter: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={styles.filter}>
+          <input type="search" onChange={this.filterHandler.bind(this)} />
+        </div>
+        {/*<ErrorBoundary>*/}
+        <Customers customers={this.state.filteredCustomers} />
+        {/*</ErrorBoundary>*/}
+      </Fragment>
     );
-  }, [filter]);
+  }
+}
 
-  const filterHandler = (event) => {
-    setFilter(event.target.value);
-  };
-
-  return (
-    <Fragment>
-      <div className={styles.filter}>
-        <input type="search" onChange={filterHandler} />
-      </div>
-
-      <Customers customers={filteredCustomers} />
-    </Fragment>
-  );
-};
+// const CustomerFilter = () => {
+//   const [filteredCustomers, setFilteredCustomers] = useState(DUMMY_CUSTOMERS);
+//   const [filter, setFilter] = useState('');
+//
+//   useEffect(() => {
+//     setFilteredCustomers(
+//       DUMMY_CUSTOMERS.filter((customer) => customer.name.includes(filter))
+//     );
+//   }, [filter]);
+//
+//   const filterHandler = (event) => {
+//     setFilter(event.target.value);
+//   };
+//
+//   return (
+//     <Fragment>
+//       <div className={styles.filter}>
+//         <input type="search" onChange={filterHandler} />
+//       </div>
+//
+//       <Customers customers={filteredCustomers} />
+//     </Fragment>
+//   );
+// };
 
 export default CustomerFilter;
